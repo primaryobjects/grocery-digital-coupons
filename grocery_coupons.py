36b96ce0b1ce54aaa61c22cc51371ade5cbff378
Kory Becker <kbecker@primaryobjects.com>
2022-10-02 20:40:16 -0400

    Fixed load to card after login.

--- grocery_coupons.py
+++ grocery_coupons.py
@@ -145,7 +145,10 @@ def shoprite(email, password, phone = None, delay = 10, callback = None):
 
                     # Reload the browser, since we're already logged in.
                     browser.refresh()
-                    time.sleep(5)
+
+                    # Just switch to iframe and continue.
+                    WebDriverWait(browser, delay).until(EC.frame_to_be_available_and_switch_to_it((By.ID, "sr-digital-coupons")))
+
                     i = i + 1
                 else:
                     break
