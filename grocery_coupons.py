53f5527c4aa582b06410e5fe2c033fc31f0b963f
Kory Becker <kbecker@primaryobjects.com>
2022-10-11 11:05:29 -0400

    fix

--- grocery_coupons.py
+++ grocery_coupons.py
@@ -145,6 +145,7 @@ def shoprite(email, password, phone = None, delay = 10, callback = None):
 
                     # Reload the browser, since we're already logged in.
                     browser.refresh()
+                    time.sleep(4)
 
                     # Just switch to iframe and continue.
                     WebDriverWait(browser, delay).until(EC.frame_to_be_available_and_switch_to_it((By.ID, "sr-digital-coupons")))
